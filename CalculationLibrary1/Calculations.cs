8e631cdc5f06eb7528eacecea41ea2528b915073
Lovepreet Kaur <klovepreet121999@gmail.com>
2022-12-11 23:23:51 -0500

    Add project files.

---
+++ CalculationLibrary1/Calculations.cs
@@ -0,0 +1,35 @@
+﻿namespace CalculationLibrary1
+{
+    public class Calculations
+    
+   
+        {
+            public static double Addition(double a, double b)
+            {
+                return a + b;
+            }
+            public static double Subtraction(double a, double b)
+            {
+                return a - b;
+            }
+            public static double Multiplication(double a, double b)
+            {
+                return a * b;
+            }
+            public static double Dividation(double a, double b)
+            {
+                if (b != 0)
+                {
+                    return a / b;
+                }
+                else
+                {
+                    throw new DivideByZeroException();
+
+                }
+            }
+        }
+    
+
+}
+}
